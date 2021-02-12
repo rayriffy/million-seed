8bc388afec059bad0598215108ec6039f376d132
Phumrapee Limpianchop <git@rayriffy.com>
2021-02-12 12:45:15 +0900

    chore: staged changes

--- src/app.ts
+++ src/app.ts
@@ -10,12 +10,14 @@ import { getIdols } from './functions/getIdols'
   // Get lists of idols
   const idols = await getIdols(browser)
 
-  //
+  // Parse detailed idol
   const dump = await Promise.all(idols.map(async idol => {
     const page = await browser.newPage()
 
     await page.goto(`https://imas.gamedbs.jp/mlth/`, {
       waitUntil: 'networkidle0'
     })
+
+    // get all character profiles
   }))
 })()
